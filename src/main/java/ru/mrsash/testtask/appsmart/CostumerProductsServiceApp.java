f73eadd763e033350ba3ff5a7de69463f015a951
MrSash <sanchess96@gmail.com>
2021-10-24 15:17:27 +0300

    TASK-1: add yml files and spring starter modules in project

---
+++ src/main/java/ru/mrsash/testtask/appsmart/CostumerProductsServiceApp.java
@@ -0,0 +1,12 @@
+package ru.mrsash.testtask.appsmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CostumerProductsServiceApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CostumerProductsServiceApp.class, args);
+    }
+}
